1a39b98adace36aa06c83f66f34d531ee1aa5902
Andy Matthews <antt@noc.ac.uk>
2024-11-27 11:23:56 +0000

    Added some useful comments

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -10,15 +10,19 @@ graph_file = './cumulative_eva_graph.png'
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
 eva_df = pd.read_json(input_file, convert_dates=['date'])
+# Pandas doesn't import eva column as a float - fix
 eva_df['eva'] = eva_df['eva'].astype(float)
+# Drop cases where eva didn't actually occur
 eva_df.dropna(axis=0, inplace=True)
 eva_df.sort_values('date', inplace=True)
 
 eva_df.to_csv(output_file, index=False)
 
+# Calculate duration of each spacewalk, and cumulative total time
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1]) / 60)
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
 
+# Create plot of graph
 plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
